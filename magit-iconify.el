b98694449216679599a01cb056b2bea860c6e916
Justin Barclay <git@justinbarclay.ca>
2025-02-07 12:02:18 -0800

    feat: use a more brittle but deterministic iconify approach
    This new approach is more brittle because it replaces functions
    defined in magit through advice. This means that if those functions
    change, at best our methods become out of sync and at worst outright
    crash. However, this does have the benefit of more reliably placing
    icons where they need to be.
    
    Use at your own risk.

--- magit-iconify.el
+++ magit-iconify.el
@@ -1,8 +1,9 @@
 ;;; magit-iconify.el --- Make you magit status buffer look iconic  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2024  Justin Barclay
+;; Copyright (C) 2024-2025  Justin Barclay
 
 ;; Author: Justin Barclay <github@justincbarclay.ca>
+;; Package-Requires: ((emacs "29.1") (magit))
 ;; Keywords:
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -23,50 +24,178 @@
 ;;
 
 ;;; Code:
-
 (require 'magit)
-(require 'nerd-icons)
-(require 'text-properties)
 
-(defun magit-iconify--insert-icon (file)
-  (if (directory-name-p file)
-      (insert (format "%s " (nerd-icons-icon-for-dir file)))
-    (insert (format "%s " (nerd-icons-icon-for-file file)))))
+(defcustom magit-iconify-fn nil
+  "An optional function that returns an icon given a file name"
+  :type '(function)
+  :group 'magit)
 
-;; magit-diff-insert-file-section
-;; magit-diff-wash-diffstat
-;; magit-diff-insert-file-section
-(defun magit-iconify--diff-file-heading ()
-  (progn
-    (forward-word)
-    (forward-whitespace 1)
-    (insert (propertize
-             (format "%s " (nerd-icons-icon-for-file
-                            (thing-at-point 'filename t)))
-             'font-lock-faces 'magit-diff-file-heading))
-    (when (re-search-forward "->\\\s+" (pos-eol) t)
-      (magit-iconify--insert-icon (thing-at-point 'filename t)))))
+(defgroup magit-iconify nil
+  "Make your magit status buffer look iconic"
+  :group 'magit)
 
-(defun magit-iconify-status-buffer ()
-  (require 'text-property-search)
-  (require 'nerd-icons)
-  (let ((pos (point)))
-    (read-only-mode -1)
-    (goto-char (point-min))
-    (while-let ((prop (text-property-search-forward 'font-lock-face nil
-                                                    (lambda (propa propb)
-                                                      (memq propb '(magit-filename magit-diff-file-heading)))
-                                                    nil)))
-      (save-mark-and-excursion
-        (goto-char (prop-match-beginning prop))
-        (if (eq (get-text-property (point) 'font-lock-face)
-                'magit-diff-file-heading)
-            ;; Move to beginning of filename
-            (magit-iconify--diff-file-heading)
-          (magit-iconify--insert-icon (thing-at-point 'filename t)))))
-    (goto-char pos)))
+(cond ((and (not magit-iconify-fn)
+            (featurep 'nerd-icons))
+       (require 'nerd-icons)
+       (eval-when-compile
+         (declare-function nerd-icons-icon-for-dir "ext:nerd-icons" t t)
+         (declare-function nerd-icons-icon-for-file "ext:nerd-icons" t t))
+       (defun magit-iconify-nerd-icon (file-or-dir)
+         "Return the nerd-icon for a file or directory"
+         (if (directory-name-p file-or-dir)
+             (nerd-icons-icon-for-dir file-or-dir)
+           (nerd-icons-icon-for-file file-or-dir)))
+       (setopt magit-file-icon-fn 'magit-iconify-nerd-icon))
+      ((and (not magit-file-icon-fn)
+            (featurep 'all-the-icons))
+       (require 'all-the-icons)
+       (eval-when-compile
+         (declare-function all-the-icons-for-dir "ext:all-the-icons" t t)
+         (declare-function all-the-icons-for-file "ext:all-the-icons" t t))
 
-(advice-add 'magit-refresh :after 'magit-iconify-status-buffer)
+       (defun magit-iconify-all-the-icons (file-or-dir)
+         "Return the all-the-icons icon for a file or directory"
+         (if (directory-name-p file-or-dir)
+             (all-the-icons-icon-for-dir file-or-dir)
+           (all-the-icons-icon-for-file file-or-dir)))
+       (setopt magit-file-icon-fn 'all-the-icons-icon-for-file)))
+
+(defun magit-iconify-file-section (file status orig)
+  (if (boundp 'magit-file-icon-fn)
+      (format "%-10s %s %s" status (funcall magit-file-icon-fn (or orig file))
+              (if (or (not orig) (equal orig file))
+                  file
+                (format "%s -> %s %s") orig (funcall magit-file-icon-fn file) file))
+    (format "%-10s %s" status
+            (if (or (not orig) (equal orig file))
+                file
+              (format "%s -> %s" orig file)))))
+
+(defun magit-diff-insert-file-section-with-icons
+    (file orig status modes rename header binary long-status)
+  (magit-insert-section
+    ( file file
+      (or (equal status "deleted") (derived-mode-p 'magit-status-mode))
+      :source (and (not (equal orig file)) orig)
+      :header header
+      :binary binary)
+    (insert (propertize (magit-iconify-file-section file status orig)
+                        'font-lock-face 'magit-diff-file-heading))
+    (cond ((and binary long-status)
+           (insert (format " (%s, binary)" long-status)))
+          ((or binary long-status)
+           (insert (format " (%s)" (if binary "binary" long-status)))))
+    (magit-insert-heading)
+    (when modes
+      (magit-insert-section (hunk '(chmod))
+        (insert modes)
+        (magit-insert-heading)))
+    (when rename
+      (magit-insert-section (hunk '(rename))
+        (insert rename)
+        (magit-insert-heading)))
+    (magit-wash-sequence #'magit-diff-wash-hunk)))
+
+(defun magit-iconify-file (file)
+  (if (boundp 'magit-file-icon-fn)
+      (format "%s %s" (funcall magit-file-icon-fn file) file)
+    file))
+
+(defun magit-insert-files-with-icons (type fn)
+  (when-let ((files (funcall fn
+                             (and magit-buffer-diff-files
+                                  (cons "--" magit-buffer-diff-files)))))
+    (magit-insert-section section ((eval type) nil t)
+                          (magit-insert-heading (length files)
+                            (let ((title (symbol-name type)))
+                              (format "%c%s files"
+                                      (capitalize (aref title 0))
+                                      (substring title 1))))
+                          (magit-insert-section-body
+                            (let ((magit-section-insert-in-reverse t)
+                                  (limit magit-status-file-list-limit))
+                              (while (and files (> limit 0))
+                                (cl-decf limit)
+                                (let ((file (pop files)))
+                                  (magit-insert-section (file file)
+                                    (insert (propertize (magit-iconify-file file) 'font-lock-face 'magit-filename))
+                                    (insert ?\n))))
+                              (when files
+                                (magit-insert-section (info)
+                                  (insert (propertize
+                                           (format "%s files not listed\n" (length files))
+                                           'face 'warning)))))
+                            (insert ?\n)
+                            (oset section children (nreverse (oref section children)))))))
+
+(defun magit-diff-wash-diffstat-with-icons ()
+  (let (heading (beg (point)))
+    (when (re-search-forward "^ ?\\([0-9]+ +files? change[^\n]*\n\\)" nil t)
+      (setq heading (match-string 1))
+      (magit-delete-match)
+      (goto-char beg)
+      (magit-insert-section (diffstat)
+        (insert (propertize heading 'font-lock-face 'magit-diff-file-heading))
+        (magit-insert-heading)
+        (let (files)
+          (while (looking-at "^[-0-9]+\t[-0-9]+\t\\(.+\\)$")
+            (push (magit-decode-git-path
+                   (let ((f (match-string 1)))
+                     (cond
+                      ((string-match "{.* => \\(.*\\)}" f)
+                       (replace-match (match-string 1 f) nil t f))
+                      ((string-match " => " f)
+                       (substring f (match-end 0)))
+                      (t f))))
+                  files)
+            (magit-delete-line))
+          (setq files (nreverse files))
+          (while (looking-at magit-diff-statline-re)
+            (magit-bind-match-strings (file sep cnt add del) nil
+              (magit-delete-line)
+              (when (string-match " +$" file)
+                (setq sep (concat (match-string 0 file) sep))
+                (setq file (substring file 0 (match-beginning 0))))
+              (let ((le (length file)) ld)
+                (setq file (magit-decode-git-path file))
+                (setq ld (length file))
+                (when (> le ld)
+                  (setq sep (concat (make-string (- le ld) ?\s) sep))))
+              (magit-insert-section (file (pop files))
+                (insert (propertize (magit-iconify-file file) 'font-lock-face 'magit-filename)
+                        sep cnt " ")
+                (when add
+                  (insert (propertize add 'font-lock-face
+                                      'magit-diffstat-added)))
+                (when del
+                  (insert (propertize del 'font-lock-face
+                                      'magit-diffstat-removed)))
+                (insert "\n")))))
+        (if (looking-at "^$") (forward-line) (insert "\n"))))))
+
+(defun override-magit-diff-insert-file-section (_orig &rest args)
+  (apply 'magit-diff-insert-file-section-with-icons args))
+
+(defun override-magit-diff-wash-diffstat (_orig &rest args)
+  (apply 'magit-diff-wash-diffstat-with-icons args))
+
+(defun override-magit-insert-files (_orig &rest args)
+  (apply 'magit-insert-files-with-icons args))
+
+;;;###autoload
+(define-minor-mode magit-iconify-mode
+  "Make your magit status buffer look iconic"
+  :global t
+  (if magit-iconify-mode
+      (progn
+        (advice-add 'magit-diff-insert-file-section :around #'override-magit-diff-insert-file-section)
+        (advice-add 'magit-diff-wash-diffstat :around #'override-magit-diff-wash-diffstat)
+        (advice-add 'magit-insert-files :around #'override-magit-insert-files))
+    (advice-remove 'magit-diff-insert-file-section #'override-magit-diff-insert-file-section)
+    (advice-remove 'magit-diff-wash-diffstat #'override-magit-diff-wash-diffstat)
+    (advice-remove 'magit-insert-files #'override-magit-insert-files))
+  :group 'magit-iconify)
 
 (provide 'magit-iconify)
 ;;; magit-iconify.el ends here
